a3a65c036631652da982d37c41662e86157f47ab
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-09-24 17:46:47 -0400

    fix

--- deepstream_load_test.js
+++ deepstream_load_test.js
@@ -7,7 +7,7 @@ meteorDown.init(function (Meteor) {
 });
 
 meteorDown.run({
-  concurrency: process.env.LOAD_TEST_VOLUME || 10,
+  concurrency: parseInt(process.env.LOAD_TEST_VOLUME) || 10,
   url: process.env.LOAD_TEST_TARGET || "http://localhost:3000",
   key: process.env.METEOR_DOWN_KEY,
   auth: process.env.METEOR_DOWN_USERID_1 ? {userIds: [process.env.METEOR_DOWN_USERID_1, process.env.METEOR_DOWN_USERID_2] }
