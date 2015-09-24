c1a4442c62367167b3d25c70c9dab10aab4ebb3b
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-09-24 19:37:57 -0400

    slightly more realistic load test

--- deepstream_load_test.js
+++ deepstream_load_test.js
@@ -1,14 +1,21 @@
 console.log('LOAD TEST GOOOOOOO!')
 
+// visit homepage and a single deepstream, then watch for 60 seconds
 meteorDown.init(function (Meteor) {
-  Meteor.subscribe('deepstreamsOnAir', function (error, result) {
-    Meteor.kill();
-  });
+	Meteor.subscribe('deepstreamsOnAir', function (error, result) {
+		Meteor.subscribe('bestStreams', function (error, result) {
+			Meteor.subscribe('singleDeepstream', 'curat0r', '3zruKDSm', function (error, result) {
+				setTimeout(function() {
+					Meteor.kill();
+				}, 60000);
+			});
+		});
+	});
 });
 
 meteorDown.run({
-  concurrency: parseInt(process.env.LOAD_TEST_VOLUME) || 10,
-  url: process.env.LOAD_TEST_TARGET || "http://localhost:3000",
-  key: process.env.METEOR_DOWN_KEY,
-  auth: process.env.METEOR_DOWN_USERID_1 ? {userIds: [process.env.METEOR_DOWN_USERID_1, process.env.METEOR_DOWN_USERID_2] } : null
+	concurrency: parseInt(process.env.LOAD_TEST_VOLUME) || 10,
+	url: process.env.LOAD_TEST_TARGET || "http://localhost:3000",
+	key: process.env.METEOR_DOWN_KEY,
+	auth: process.env.METEOR_DOWN_USERID_1 ? {userIds: [process.env.METEOR_DOWN_USERID_1, process.env.METEOR_DOWN_USERID_2] } : null
 });
