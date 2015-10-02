555933eef9b3e7e58c00fd58b2ffad6427868f19
Joseph Goldbeck <goldbeck.joseph@gmail.com>
2015-10-01 20:35:10 -0400

    confirm changes are picked up

--- deepstream_load_test.js
+++ deepstream_load_test.js
@@ -1,7 +1,24 @@
 console.log('LOAD TEST GOOOOOOO!')
 
+
 // visit homepage and a single deepstream, then watch for 60 seconds
 meteorDown.init(function (Meteor) {
+
+	var observer = Meteor.observe("deepstreams");
+	  observer.added = function(id) {
+	    console.log("[ADDED] to " + observer.name + ":  " + id);
+	  };
+	  observer.changed = function(id, oldFields, clearedFields, newFields) {
+	    console.log("[CHANGED] in " + observer.name + ":  " + id);
+	    console.log("[CHANGED] old field values: ", oldFields);
+	    console.log("[CHANGED] cleared fields: ", clearedFields);
+	    console.log("[CHANGED] new fields: ", newFields);
+	  };
+	  observer.removed = function(id, oldValue) {
+	    console.log("[REMOVED] in " + observer.name + ":  " + id);
+	    console.log("[REMOVED] previous value: ", oldValue);
+	  };
+
 	Meteor.subscribe('deepstreamsOnAir', function (error, result) {
 		Meteor.subscribe('deepstreamContext', '3zruKDSm', function (error, result) {
 			Meteor.subscribe('singleDeepstream', 'curat0r', '3zruKDSm', function (error, result) {
@@ -12,6 +29,7 @@ meteorDown.init(function (Meteor) {
 		});
 	});
 });
+
 
 meteorDown.run({
 	concurrency: parseInt(process.env.LOAD_TEST_VOLUME) || 10,
